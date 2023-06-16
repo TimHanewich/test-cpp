3cab26553a0dc97ad5f6d33e6705f230e64fa912
Tim Hanewich <tahanewich@live.com>
2023-06-16 18:04:16 -0400

    Delcare main with type

--- main.cpp
+++ main.cpp
@@ -5,12 +5,13 @@
 using namespace std;
 
 
-main()
+int main()
 {
     float float1 = 0.25;
     float float2 = 0.50;
     float result = add_floats(0.5, 0.45);
     std::cout << to_string(result);
+    return 0;
 }
 
 
